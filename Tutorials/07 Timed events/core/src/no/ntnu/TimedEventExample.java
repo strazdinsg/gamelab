025db5cb3f25e9518dfede357217ff4937af8f48
Girts <gist@ntnu.no>
2018-01-24 14:20:20 +0100

    Another alternative added: counting time manually

--- Tutorials/07 Timed events/core/src/no/ntnu/TimedEventExample.java
+++ Tutorials/07 Timed events/core/src/no/ntnu/TimedEventExample.java
@@ -1,6 +1,7 @@
 package no.ntnu;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 
 /**
  * This applications shows how to implement timed events: something happening at specific time intervals
@@ -10,6 +11,8 @@ public class TimedEventExample extends ApplicationAdapter {
     private EventTimer timer2;
     private EventTimer timer3;
 
+    float accumulatedTime; // We will use this to manually count time since last event
+
     @Override
     public void create() {
         timer1 = new EventTimer();
@@ -18,10 +21,13 @@ public class TimedEventExample extends ApplicationAdapter {
         timer1.start(1000, true); // This one will fire every second (1000 milliseconds)
         timer2.start(2700, true); // this one will fire every 2.7 seconds (2700 milliseconds)
         timer3.start(10000, false); // This one will fire only once: after 10 sec, and will stop the timer 1
+
+        accumulatedTime = 0; // We start counting now
     }
 
     @Override
     public void render() {
+        // Alternative #1: Use EventTimer class
         if (timer1.mustFire()) {
             System.out.println("Timer 1 fired");
         }
@@ -31,6 +37,14 @@ public class TimedEventExample extends ApplicationAdapter {
         if (timer3.mustFire()) {
             System.out.println("Timer 3 fired");
             timer1.stop();
+        }
+
+        // Alternative #2: Count time since last event manually
+        float secondsSinceLastFrame = Gdx.graphics.getDeltaTime();
+        accumulatedTime += secondsSinceLastFrame;
+        if (accumulatedTime >= 5.0) {
+            System.out.println("Five seconds have passed since last checkpoint");
+            accumulatedTime = 0;
         }
     }
 
