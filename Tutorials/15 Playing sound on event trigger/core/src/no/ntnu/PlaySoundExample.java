c8af01a7531b315d57b817488a314091866b4265
osteinnes <olemartinhs@hotmail.com>
2018-01-26 19:40:53 +0100

    Tutorial #22: Finished

--- Tutorials/15 Playing sound on event trigger/core/src/no/ntnu/PlaySoundExample.java
+++ Tutorials/15 Playing sound on event trigger/core/src/no/ntnu/PlaySoundExample.java
@@ -6,27 +6,27 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 
 public class PlaySoundExample extends ApplicationAdapter {
-    
-    private PlayAudio playAudio;
+
+	private PlayAudio playAudio;
 
 	@Override
-        public void create () {
-                Gdx.input.setInputProcessor(new InputHandler());
+	public void create () {
+		Gdx.input.setInputProcessor(new InputHandler());
 
-                playAudio = new PlayAudio();
-        }
+		playAudio = new PlayAudio();
+	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-                
-                // Play sound if W is pressed.
-                if (Gdx.input.isKeyJustPressed(Input.Keys.W)){
-                   playAudio.playSound("nine.ogg");
-                }
+
+		// Play sound if W is pressed.
+		if (Gdx.input.isKeyJustPressed(Input.Keys.W)){
+			playAudio.playSound("nine.ogg");
+		}
 	}
-	
+
 	@Override
 	public void dispose () {
 	}
