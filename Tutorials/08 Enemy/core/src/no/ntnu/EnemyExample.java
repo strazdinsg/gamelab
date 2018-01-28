cfed731977d2bc7ff0e80d90e7990542d332515d
Girts <gist@ntnu.no>
2018-01-29 00:04:29 +0100

    Example 08 done - enemy roaming in the game world

--- Tutorials/08 Enemy/core/src/no/ntnu/EnemyExample.java
+++ Tutorials/08 Enemy/core/src/no/ntnu/EnemyExample.java
@@ -1,11 +1,11 @@
 package no.ntnu;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 
 /**
  * This example shows how to create an enemy that roams in the game world
@@ -17,9 +17,9 @@ public class EnemyExample extends ApplicationAdapter {
     public static final int SCREEN_WIDTH = 800;
     public static final int SCREEN_HEIGHT = 600;
 
-    // Enemy size, in pixels
-    private static final int ENEMY_WIDTH = 64;
-    private static final int ENEMY_HEIGHT = 54;
+    // Enemy speed, pixels per second
+    public static final float ENEMY_SPEED_X = 300;
+    public static final float ENEMY_SPEED_Y = -100 - 300 * MathUtils.random();
 
     // Used for a 2D projection of the game world
     private OrthographicCamera camera;
@@ -28,50 +28,59 @@ public class EnemyExample extends ApplicationAdapter {
     private SpriteBatch batch;
 
     // Coordinates for enemy
-    Rectangle enemy; 
-    
-    // Texture for enemy
-    Texture enemyImage;
+    private Enemy enemy;
 
     @Override
     public void create() {
-        // Set our camera to display a 2D world, 800x600 pixels large 
-        // (or whatever the screen size is set to curently)
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
-        camera.update();
+        createCamera();
 
         // Prepare to draw 2D sprites
         batch = new SpriteBatch();
-        
-        // Load texture for the enemy
-        enemyImage = new Texture("enemy.png");
 
-        spawnEnemy();
+        createEnemy();
     }
 
     @Override
     public void render() {
+        // Clear the screen
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         // Start our sprite drawing
         batch.begin();
-        batch.draw(enemyImage, enemy.x, enemy.y);
+
+        // Call one render/update loop for the enemy. This call will do all 
+        // the logic and also drawing of the enemy
+        enemy.updateAndRender(batch);
+
         // We are done with sprite drawing, allow the system to put them on screen
         batch.end();
     }
 
     @Override
     public void dispose() {
+        // Release the reserved memory resources
+        enemy.dispose();
     }
 
     /**
      * Creates an enemy
      */
-    private void spawnEnemy() {
-        enemy = new Rectangle();
-        // Position the enemy in the top of the screen, randomly on the X axis
-        enemy.x = MathUtils.random(0, SCREEN_WIDTH - ENEMY_WIDTH);
-        enemy.y = SCREEN_HEIGHT - ENEMY_HEIGHT;
-        enemy.width = ENEMY_WIDTH;
-        enemy.height = ENEMY_HEIGHT;
+    private void createEnemy() {
+        // Spawn it at the top of the screen, at a random horizontal position
+        final int BOUNDARY = 100;
+        int enemyX = MathUtils.random(0, SCREEN_WIDTH - BOUNDARY);
+        int enemyY = SCREEN_HEIGHT - BOUNDARY;
+        enemy = new Enemy("enemy.png", enemyX, enemyY, ENEMY_SPEED_X, ENEMY_SPEED_Y);
+    }
+
+    /**
+     * Set our camera to display a 2D world, 800x600 pixels large
+     * (or whatever the screen size is set to currently)
+     */
+    private void createCamera() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
+        camera.update();
     }
 }
