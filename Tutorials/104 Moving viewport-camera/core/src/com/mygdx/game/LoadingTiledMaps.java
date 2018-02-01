7f29d289eb272b5f6862e7edd2219d1fcac8b4d7
osteinnes <olemartinhs@hotmail.com>
2018-02-01 21:01:42 +0100

    Tutorial #104: Moves through tile map, based on keypress (A and D). It works, but ain't finished yet.

---
+++ Tutorials/104 Moving viewport-camera/core/src/com/mygdx/game/LoadingTiledMaps.java
@@ -0,0 +1,90 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+/**
+ * Wiki: https://github.com/strazdinsg/gamelab/wiki/Tile-maps
+ */
+public class LoadingTiledMaps {
+
+    // This is used to load our map
+    private TmxMapLoader mapLoader;
+
+    // Used as a reference to our map
+    private TiledMap map;
+
+    // Used to render our orthogonal map to the screen.
+    private OrthogonalTiledMapRenderer mapRenderer;
+
+    // The camera that captures the map.
+    private OrthographicCamera gameCamera;
+
+    // Since each tile is 16px, and we want each tile to represent a unit we scale it down by 1/16f.
+    public final static float unitScale = 1 / 16f;
+
+    /**
+     * LoadingTiledMaps object
+     *
+     * @param filepath The map filepath
+     * @throws NullPointerException if filepath is not found.
+     */
+    public LoadingTiledMaps(String filepath) throws NullPointerException {
+        loadMap(filepath);
+        createRenderer();
+        createCamera();
+    }
+
+    /**
+     * Renders map.
+     */
+    public void render() {
+        mapRenderer.setView(gameCamera);
+        mapRenderer.render();
+    }
+
+    /**
+     * Disposes of map
+     */
+    public void dispose() {
+        map.dispose();
+    }
+
+    /**
+     * Loads map.
+     *
+     * @param filepath Filepath of the map.
+     */
+    private void loadMap(String filepath) {
+        // Loads map
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load(filepath);
+    }
+
+    /**
+     * Create new map renderer.
+     */
+    private void createRenderer() {
+        // We will use this to render the map (in render method)
+        mapRenderer = new OrthogonalTiledMapRenderer(map, unitScale);
+    }
+
+    /**
+     * Create new camera.
+     */
+    private void createCamera() {
+        gameCamera = new OrthographicCamera();
+
+        // Sets the camera up to 20 units with and 13 unit height. (Read Wiki)
+        gameCamera.setToOrtho(false, 20, 13);
+    }
+
+    /**
+     * @return game camera
+     */
+    public OrthographicCamera getGameCamera() {
+        return gameCamera;
+    }
+}
--- Tutorials/104 Moving viewport-camera/core/src/com/mygdx/game/ViewportExample.java
+++ Tutorials/104 Moving viewport-camera/core/src/com/mygdx/game/ViewportExample.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -16,29 +17,15 @@ public class ViewportExample extends Game {
     private Camera camera;
 
     // The virtual screen width and height.
-    private final static int HEIGHT = 600;
-    private final static int WIDTH = 800;
+    private final static int HEIGHT = 208;
+    private final static int WIDTH = 400;
 
-    // Uses a background texture and a batch to show differences between viewports.
-    private Texture texture;
-    private SpriteBatch batch;
+    private LoadingTiledMaps map;
 
     @Override
     public void create() {
-
-        /*
-        There is a use-method for each of the viewport options.
-		How these work are described in the methods themselves.
-         */
+        map = new LoadingTiledMaps("level1.tmx");
         useFitViewport();
-        //useStretchViewport();
-        //useFillViewport();
-        //useExtendViewport();
-        //useScreenViewport();
-
-        // A background texture and a batch, in order to see the black bars.
-        texture = new Texture("sky1.png");
-        batch = new SpriteBatch();
     }
 
     @Override
@@ -47,23 +34,22 @@ public class ViewportExample extends Game {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Draws the background, in order to spot black bars.
-        batch.begin();
-        batch.draw(texture, 0, 0);
-        batch.end();
+        map.render();
+
+        handleInput();
+        camera.update();
     }
 
     @Override
     public void resize(int width, int height) {
         // When the game window gets resized, the viewport must me told.
-        viewport.update(width, height);
+        viewport.update(width,height);
     }
 
     @Override
     public void dispose() {
         // Dispose of components when we are done with them.
-        texture.dispose();
-        batch.dispose();
+        map.dispose();
     }
 
     /**
@@ -73,71 +59,22 @@ public class ViewportExample extends Game {
     private void useFitViewport() {
         // A viewport manages a cameras viewportWidth and viewportHeight.
         // Thus it needs a camera to be supplied the constructor.
-        camera = new OrthographicCamera();
-        viewport = new FitViewport(WIDTH, HEIGHT, camera);
+        camera = map.getGameCamera();
+        viewport = new FitViewport(WIDTH * map.unitScale, HEIGHT * map.unitScale, camera);
 
         // Applies every render that happens, to the viewport.
         viewport.apply();
     }
 
     /**
-     * Stretch viewport maintains the original width and height, but not the
-     * aspect-ratio.
-     * This will result in textures looking stretched on some screens.
+     * When Right or Left key is pressed, camera is moved in the
+     * corresponding direction.
      */
-    private void useStretchViewport() {
-        // A viewport manages a cameras viewportWidth and viewportHeight.
-        // Thus it needs a camera to be supplied the constructor.
-        camera = new OrthographicCamera();
-        viewport = new StretchViewport(WIDTH, HEIGHT, camera);
-
-        // Applies every render that happens, to the viewport.
-        viewport.apply();
+    private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            camera.position.x += 0.1;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            camera.position.x -= 0.4;
+        }
     }
-
-    /**
-     * Fill viewport maintains the original aspect-ratio, but fills the screen.
-     * This results
-     * in some of the viewport might not show on screen, depending on it's size.
-     */
-    private void useFillViewport() {
-        // A viewport manages a cameras viewportWidth and viewportHeight.
-        // Thus it needs a camera to be supplied the constructor.
-        camera = new OrthographicCamera();
-        viewport = new FillViewport(WIDTH, HEIGHT, camera);
-
-        // Applies every render that happens, to the viewport.
-        viewport.apply();
-    }
-
-    /**
-     * Extend viewport maintains aspect ratio without black bars, by extending
-     * the world in one direction.
-     */
-    private void useExtendViewport() {
-        // A viewport manages a cameras viewportWidth and viewportHeight.
-        // Thus it needs a camera to be supplied the constructor.
-        camera = new OrthographicCamera();
-        viewport = new ExtendViewport(WIDTH, HEIGHT, camera);
-
-        // Applies every render that happens, to the viewport.
-        viewport.apply();
-    }
-
-    /**
-     * Screen viewport doesn't have virtual width or height. Therefore, no
-     * scaling or black bars appear.
-     * This results in the game looking entirely different, on different
-     * screens.
-     */
-    private void useScreenViewport() {
-        // A viewport manages a cameras viewportWidth and viewportHeight.
-        // Thus it needs a camera to be supplied the constructor.
-        camera = new OrthographicCamera();
-        viewport = new ScreenViewport(camera);
-
-        // Applies every render that happens, to the viewport.
-        viewport.apply();
-    }
-
 }
