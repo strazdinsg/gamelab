960dbcd2e14c75273b3b8834d762cd7068305865
girts <girts.strazdins@gmail.com>
2018-02-15 23:31:27 +0100

    Spawn enemies probabilistically

--- Tutorials/10 Spawn Enemies/core/src/no/ntnu/EnemyExample.java
+++ Tutorials/10 Spawn Enemies/core/src/no/ntnu/EnemyExample.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,9 +33,25 @@ public class EnemyExample extends ApplicationAdapter {
     private static final float MAX_X = SCREEN_WIDTH - SAFE_BOUNDARY;
     private static final float MAX_Y = SCREEN_HEIGHT - SAFE_BOUNDARY;
 
-    // Number of enemies of each type
-    public static final int STRAIGHT_ENEMY_COUNT = 3;
-    public static final int JITTERING_ENEMY_COUNT = 2;
+    // Probability of spawning each type of enemy: odds to spawn it once per second
+    // Each second we have X% chance to spawn a straight-moving enemy 
+    // and Y% chance to spawn a jittering enemy
+    private static final float STRAIGHT_SPAWN_PROBABILITY = 0.3f;
+    private static final float JITTER_SPAWN_PROBABILITY = 0.1f;
+
+    // Max number of enemies of each type
+    private static final int MAX_STRAIGHT_ENEMIES = 10;
+    private static final int MAX_JITTERING_ENEMIES = 6;
+
+    // Current number of enemies
+    private int straightEnemyCount = 0;
+    private int jitteringEnemyCount = 0;
+
+    // Time when the last enemy was spawned
+    private long lastSpawnTime = 0;
+
+    // How many milliseconds must pass between two spawns of enemies
+    private static final long SPAWN_COOLDOWN_TIME = 3000;
 
     // Used for a 2D projection of the game world
     private OrthographicCamera camera;
@@ -43,14 +60,13 @@ public class EnemyExample extends ApplicationAdapter {
     private SpriteBatch batch;
 
     // Coordinates for enemy
-    private List<Enemy> enemies;
+    private final List<Enemy> enemies = new ArrayList<Enemy>();
 
     @Override
     public void create() {
         createCamera();
         // Prepare to draw 2D sprites
         batch = new SpriteBatch();
-        createEnemies();
     }
 
     @Override
@@ -61,6 +77,8 @@ public class EnemyExample extends ApplicationAdapter {
 
         // Start our sprite drawing
         batch.begin();
+
+        trySpawnEnemies();
 
         // Call one render/update loop for each enemy. This call will do all 
         // the logic and also drawing of the enemies
@@ -81,39 +99,6 @@ public class EnemyExample extends ApplicationAdapter {
     }
 
     /**
-     * Creates the enemies
-     */
-    private void createEnemies() {
-        enemies = new ArrayList<Enemy>();
-
-        // Let's create some straight-moving enemies
-        for (int i = 1; i <= STRAIGHT_ENEMY_COUNT; ++i) {
-            // Spawn enemies at random position, with random speed
-            float enemyX = MathUtils.random(MIN_X, MAX_X);
-            float enemyY = MathUtils.random(MIN_Y, MAX_Y);
-            // Movement allowed both ways - also in negative direction
-            float speedX = MathUtils.random(-MAX_SPEED_X, MAX_SPEED_X);
-            float speedY = MathUtils.random(-MAX_SPEED_Y, MAX_SPEED_Y);
-            Enemy e = new Enemy("enemy.png", enemyX, enemyY);
-            e.setMovementAI(new StraightMovementAI(enemyX, enemyY,
-                    MIN_X, MAX_X, MIN_Y, MAX_Y, speedX, speedY));
-            enemies.add(e);
-        }
-
-        // Let's create some jittering enemies
-        for (int i = 1; i <= JITTERING_ENEMY_COUNT; ++i) {
-            // Spawn enemies at random position, with random speed
-            float enemyX = MathUtils.random(MIN_X, MAX_X);
-            float enemyY = MathUtils.random(MIN_Y, MAX_Y);
-            // Movement allowed both ways - also in negative direction
-            Enemy e = new Enemy("zombie.png", enemyX, enemyY);
-            e.setMovementAI(new JitteringMovementAI(enemyX, enemyY,
-                    MIN_X, MAX_X, MIN_Y, MAX_Y, MAX_SPEED_X, MAX_SPEED_Y));
-            enemies.add(e);
-        }
-    }
-
-    /**
      * Set our camera to display a 2D world, 800x600 pixels large
      * (or whatever the screen size is set to currently)
      */
@@ -121,5 +106,63 @@ public class EnemyExample extends ApplicationAdapter {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
         camera.update();
+    }
+
+    /**
+     * Spawn enemies with a certain probability
+     */
+    private void trySpawnEnemies() {
+        // Check if enough time passed since last spawn
+        long currentTime = TimeUtils.millis();
+        if (currentTime - lastSpawnTime < SPAWN_COOLDOWN_TIME) {
+            return;
+        }
+
+        // Throw a die - if it is withing the allowed probability range, 
+        // spawn an enemy of given type
+        float die = MathUtils.random(1.0f);
+        // Adjust the probability to the framerate, to get 1/second unit
+        die /= Gdx.graphics.getDeltaTime();
+
+        // We should start with the smallest probability first, otherwise the
+        // most rare enemy will never be spawned
+        if (die <= JITTER_SPAWN_PROBABILITY
+                && jitteringEnemyCount < MAX_JITTERING_ENEMIES) {
+            spawnEnemy("zombie.png", false);
+            ++jitteringEnemyCount;
+            lastSpawnTime = TimeUtils.millis();
+        } else if (die <= STRAIGHT_SPAWN_PROBABILITY
+                && straightEnemyCount < MAX_STRAIGHT_ENEMIES) {
+            spawnEnemy("enemy.png", true);
+            ++straightEnemyCount;
+            lastSpawnTime = TimeUtils.millis();
+        }
+    }
+
+    /**
+     * Spawn enemy with given texture and movement patter
+     *
+     * @param texture
+     * @param movesStraight when true, spawn a straight-moving enemy; a
+     * jittery one otherwise
+     */
+    private void spawnEnemy(String texture, boolean movesStraight) {
+        // Spawn enemies at random position, with random speed
+        float enemyX = MathUtils.random(MIN_X, MAX_X);
+        float enemyY = MathUtils.random(MIN_Y, MAX_Y);
+        // Movement allowed both ways - also in negative direction
+        float speedX = MathUtils.random(-MAX_SPEED_X, MAX_SPEED_X);
+        float speedY = MathUtils.random(-MAX_SPEED_Y, MAX_SPEED_Y);
+        Enemy e = new Enemy(texture, enemyX, enemyY);
+        MovementAI ai;
+        if (movesStraight) {
+            ai = new StraightMovementAI(enemyX, enemyY, MIN_X, MAX_X, MIN_Y,
+                    MAX_Y, speedX, speedY);
+        } else {
+            ai = new JitteringMovementAI(enemyX, enemyY, MIN_X, MAX_X, MIN_Y,
+                    MAX_Y, MAX_SPEED_X, MAX_SPEED_Y);
+        }
+        e.setMovementAI(ai);
+        enemies.add(e);
     }
 }
